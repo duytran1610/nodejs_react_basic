21406ad2a72993e715da458d34271d2f85cc7b12
duytran1610 <dukhoa4.clknn@gmail.com>
2023-07-28 08:52:26 +0700

    Mapping State (Redux) to Props (React)

--- src/resourses/views/Home/Home.js
+++ src/resourses/views/Home/Home.js
@@ -1,11 +1,13 @@
 import { Component } from "react";
 import Color from "../HOC/Color";
 import logo from "../../../public/img/nodejs.png";
+import { connect } from "react-redux";
 
 class Home extends Component {
     
 
     render() {
+        console.log('>>>check dataRedux', this.props.dataRedux);
         return (
             <>
                 <div>Home</div>
@@ -17,4 +19,12 @@ class Home extends Component {
     }
 }
 
-export default Color(Home);
+// Create function nhận state và trả về đối tượng (sau này sẽ là props)
+const mapStatetoProps = (state) => {
+    return {
+        dataRedux: state.users
+    }
+}
+
+// Mapping State (Redux) to Props (React) by connect()
+export default connect(mapStatetoProps)(Color(Home));
